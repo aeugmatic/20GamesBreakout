c19d545a6783814305831fff620d44bb792b01d3
Kristians Vecainis <kvecainis88@gmail.com>
2026-03-31 22:22:38 +0100

    Implement input-based ball-launching
    - Fix random direction gen for ball to use -1 as the range limit for the y-component rather than 1 (since up = negative)
    - Change Ball to be a CharacterBody2D node, since it has to be affected by player input if it is to move alongside the paddle before launch
    - Add the new launch function to Ball, to be called when the launch input is pressed
    - Make ball follow the paddle before being launched

--- game/scripts/ball.gd
+++ game/scripts/ball.gd
@@ -5,23 +5,29 @@ signal ball_offscreen
 var direction: Vector2 = Vector2(0, 1)
 var speed: float = 180.0
 
+var count: int = 0
+
 func _physics_process(delta: float) -> void:
-	linear_velocity = speed * direction
-	var coll_info: KinematicCollision2D = move_and_collide(linear_velocity * delta)
+	var moving: bool = linear_velocity != Vector2.ZERO
 	
-	if coll_info != null:
-		direction = direction.bounce(coll_info.get_normal()).normalized()
-		linear_velocity = speed * direction
+	if moving:
+		var coll_info: KinematicCollision2D = move_and_collide(linear_velocity * delta)
+	
+		if coll_info != null:
+			direction = direction.bounce(coll_info.get_normal()).normalized()
+			linear_velocity = speed * direction
 
 func reset(pos: Vector2) -> void:
-	# random direction
+	linear_velocity = Vector2.ZERO
+	position = pos
+
+func launch() -> void:
 	direction = Vector2(
 		randf_range(-1, 1),
-		randf_range(0, 1)
+		randf_range(0, -1)
 	).normalized()
 	
 	linear_velocity = speed * direction
-	position = pos
 
 func _on_screen_exited() -> void:
 	ball_offscreen.emit()
--- game/scripts/game.gd
+++ game/scripts/game.gd
@@ -1,11 +1,22 @@
 extends Node
 
-const RESET_TIME: float = 2.5
+const RESET_TIME: float = 1.0
 
 # Initialise game
 func _ready() -> void:
-	$Ball.reset($Paddle/BallSpawn.global_position)
+	reset_ball()
+
+# FIXME: fix issue where you need to wait before the ball is parented / "anchored" to the paddle
+func reset_ball() -> void:
+	# Make the ball a child of the paddle before launch - this makes the ball exactly mimic the 
+	# paddle's movements
+	$Ball.reparent($Paddle)
+	$Paddle/Ball.reset($Paddle/BallSpawn.position)
 
 func _on_ball_offscreen() -> void:
 	await get_tree().create_timer(RESET_TIME).timeout
-	$Ball.reset($Paddle/BallSpawn.global_position)
+	reset_ball()
+
+func _on_ball_launch_pressed() -> void:
+	$Paddle/Ball.reparent(self)
+	$Ball.launch()
