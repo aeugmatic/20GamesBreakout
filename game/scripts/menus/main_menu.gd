fe763d8a1cac7c20ac127939e1d94a1e0a335eea
Kristians Vecainis <kvecainis88@gmail.com>
2026-04-09 23:48:00 +0100

    Implement game setup menu and add empty script

--- game/scripts/menus/main_menu.gd
+++ game/scripts/menus/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_game/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/game_setup_menu.tscn")
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/option_menu.tscn")
