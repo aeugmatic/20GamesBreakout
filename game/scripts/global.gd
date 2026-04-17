bed6b1c31de852efaa41a8e9ee6d65af564f1995
Kristians Vecainis <kvecainis88@gmail.com>
2026-04-17 16:14:41 +0100

    Begin difficulty setting implementation

--- game/scripts/global.gd
+++ game/scripts/global.gd
@@ -2,3 +2,7 @@ extends Node
 
 var music_vol: float = 50.0
 var sfx_vol: float = 50.0
+var game_settings: Dictionary = {
+	"ball-speedup": false,
+	"paddle-shrink": false
+}
--- game/scripts/menus/game_setup_menu.gd
+++ game/scripts/menus/game_setup_menu.gd
@@ -1,1 +1,28 @@
 extends Control
+
+const DIFF_SETTINGS: Dictionary = {
+	"easy": {
+		"ball-speedup": false,
+		"paddle_shrink": false
+	},
+	"medium": {
+		"ball-speedup": true,
+		"paddle_shrink": false
+	},
+	"hard": {
+		"ball-speedup": true,
+		"paddle_shrink": true
+	}
+}
+
+# 
+# TODO:
+# - implement option-locking for non-custom diffs
+# - implement difficulties in the gameplay itself
+
+func _ready() -> void:
+	# Set "Easy" as the default difficulty setting
+	$Panel/Options/DiffSelect/DiffButtonContainer/EasyCheckBox.button_pressed = true
+
+func set_options_based_on_diff(diff: String) -> void:
+	Global.game_settings[diff] = DIFF_SETTINGS[diff]
