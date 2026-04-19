3543297ef3faa82411811b84bfef75637339dc0a
Kristians Vecainis <kvecainis88@gmail.com>
2026-04-19 16:32:27 +0100

    Finish difficulty options UI implementation

--- game/scripts/global.gd
+++ game/scripts/global.gd
@@ -1,8 +1,29 @@
 extends Node
 
+const DIFF_SETTINGS: Dictionary = {
+	"easy": {
+		"ball-speedup": false,
+		"paddle_shrink": false
+	},
+	"medium": {
+		"ball-speedup": true,
+		"paddle_shrink": false
+	},
+	"hard": {
+		"ball-speedup": true,
+		"paddle_shrink": true
+	}
+}
+
 var music_vol: float = 50.0
 var sfx_vol: float = 50.0
-var game_settings: Dictionary = {
-	"ball-speedup": false,
-	"paddle-shrink": false
-}
+var difficulty: String = "easy"
+var game_settings: Dictionary = DIFF_SETTINGS[difficulty] # Easy as default difficulty
+
+func set_difficulty(diff: String) -> void:
+	difficulty = diff
+	if difficulty != "custom":
+		game_settings = DIFF_SETTINGS[diff]
+	
+	print("Global:")
+	print(game_settings)
--- game/scripts/menus/game_setup_menu.gd
+++ game/scripts/menus/game_setup_menu.gd
@@ -1,21 +1,5 @@
 extends Control
 
-const DIFF_SETTINGS: Dictionary = {
-	"easy": {
-		"ball-speedup": false,
-		"paddle_shrink": false
-	},
-	"medium": {
-		"ball-speedup": true,
-		"paddle_shrink": false
-	},
-	"hard": {
-		"ball-speedup": true,
-		"paddle_shrink": true
-	}
-}
-
-# 
 # TODO:
 # - implement option-locking for non-custom diffs
 # - implement difficulties in the gameplay itself
@@ -23,6 +7,61 @@ const DIFF_SETTINGS: Dictionary = {
 func _ready() -> void:
 	# Set "Easy" as the default difficulty setting
 	$Panel/Options/DiffSelect/DiffButtonContainer/EasyCheckBox.button_pressed = true
+	set_ui_diff_options()
 
-func set_options_based_on_diff(diff: String) -> void:
-	Global.game_settings[diff] = DIFF_SETTINGS[diff]
+func set_ui_diff_options() -> void:
+	# Step 1: set the UI elements
+	set_checkboxes()
+	set_game_options() # Step 2: lock UI elements if non-custom difficulty
+
+func set_checkboxes() -> void:
+	for cb in $Panel/Options/DiffSelect/DiffButtonContainer.get_children():
+		cb.button_pressed = false
+	
+	match Global.difficulty:
+		"easy":
+			$Panel/Options/DiffSelect/DiffButtonContainer/EasyCheckBox.button_pressed = true
+		"medium":
+			$Panel/Options/DiffSelect/DiffButtonContainer/MedCheckBox.button_pressed = true
+		"hard":
+			$Panel/Options/DiffSelect/DiffButtonContainer/HardCheckBox.button_pressed = true
+		"custom":
+			$Panel/Options/DiffSelect/DiffButtonContainer/CustomCheckBox.button_pressed = true
+
+func set_game_options() -> void:
+	# FIXME: tree could be re-organised to have this work with a for loop like with checkboxes
+	if Global.difficulty != "custom":
+		# for op in GameOptions.get_children():
+		#     op/SettableUI = locked
+		$Panel/Options/GameOptions/BallSpeedUpSelect/CheckButton.disabled = true
+		$Panel/Options/GameOptions/PaddleShrinkSelect/CheckButton.disabled = true
+	else:
+		$Panel/Options/GameOptions/BallSpeedUpSelect/CheckButton.disabled = false
+		$Panel/Options/GameOptions/PaddleShrinkSelect/CheckButton.disabled = false
+	
+	match Global.difficulty:
+		"easy":
+			$Panel/Options/GameOptions/BallSpeedUpSelect/CheckButton.button_pressed = false
+			$Panel/Options/GameOptions/PaddleShrinkSelect/CheckButton.button_pressed = false
+		"medium":
+			$Panel/Options/GameOptions/BallSpeedUpSelect/CheckButton.button_pressed = true
+			$Panel/Options/GameOptions/PaddleShrinkSelect/CheckButton.button_pressed = false
+		"hard":
+			$Panel/Options/GameOptions/BallSpeedUpSelect/CheckButton.button_pressed = true
+			$Panel/Options/GameOptions/PaddleShrinkSelect/CheckButton.button_pressed = true
+
+func _on_easy_check_box_pressed() -> void:
+	Global.set_difficulty("easy")
+	set_ui_diff_options()
+
+func _on_med_check_box_pressed() -> void:
+	Global.set_difficulty("medium")
+	set_ui_diff_options()
+
+func _on_hard_check_box_pressed() -> void:
+	Global.set_difficulty("hard")
+	set_ui_diff_options()
+
+func _on_custom_check_box_pressed() -> void:
+	Global.set_difficulty("custom")
+	set_ui_diff_options()
